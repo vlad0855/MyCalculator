793dfcc556ee5989b822765d40a68690620c8081
VladimirTsarev1 <vladimir.csarev@gmail.com>
2019-07-07 02:33:51 +0600

    Update Form1.cs
    Make switch

--- MyCalculator/Form1.cs
+++ MyCalculator/Form1.cs
@@ -19,72 +19,60 @@ namespace MyCalculator
 
         private void Plus_Click(object sender, EventArgs e)
         {
-            try
-            {
-                double firstArgument = double.Parse(FirstArgumentTextBox.Text);
-                double secondArgument = double.Parse(SecondArgumentTextBox.Text);
-                double result = firstArgument + secondArgument;
-                ResultTextBox.Text = result.ToString();
-            }
-            catch
-            {
-                FirstArgumentTextBox.Text = "";
-                SecondArgumentTextBox.Text = "";
-            }
-
+            Calculate(sender);
         }
 
         private void Minus_Click(object sender, EventArgs e)
         {
-            try
-            {
-                double firstArgument = double.Parse(FirstArgumentTextBox.Text);
-                double secondArgument = double.Parse(SecondArgumentTextBox.Text);
-                double result = firstArgument - secondArgument;
-                ResultTextBox.Text = result.ToString();
-            }
-            catch
-            {
-                FirstArgumentTextBox.Text = "";
-                SecondArgumentTextBox.Text = "";
-            }
-
+            Calculate(sender);
         }
 
         private void Multiplication_Click(object sender, EventArgs e)
         {
-            try
-            {
-                double firstArgument = double.Parse(FirstArgumentTextBox.Text);
-                double secondArgument = double.Parse(SecondArgumentTextBox.Text);
-
-                double result = firstArgument * secondArgument;
-                ResultTextBox.Text = result.ToString();
-            }
-            catch
-            {
-                FirstArgumentTextBox.Text = "";
-                SecondArgumentTextBox.Text = "";
-            }
-
+            Calculate(sender);
         }
 
         private void Division_Click(object sender, EventArgs e)
+        {
+            Calculate(sender);
+        }
+
+        void Calculate(object sender)
         {
             try
             {
                 double firstArgument = double.Parse(FirstArgumentTextBox.Text);
                 double secondArgument = double.Parse(SecondArgumentTextBox.Text);
-
-                double result = firstArgument / secondArgument;
-                ResultTextBox.Text = result.ToString();
+                double result;
+                switch (((Button)sender).Name)
+                {
+                    case "Plus":
+                        result = firstArgument + secondArgument;
+                        ResultTextBox.Text = result.ToString();
+                        break;
+                    case "Minus":
+                        result = firstArgument - secondArgument;
+                        ResultTextBox.Text = result.ToString();
+                        break;
+                    case "Multiplication":
+                        result = firstArgument * secondArgument;
+                        ResultTextBox.Text = result.ToString();
+                        break;
+                    case "Division":
+                        result = firstArgument / secondArgument;
+                        ResultTextBox.Text = result.ToString();
+                        break;
+                    default:
+                        FirstArgumentTextBox.Text = "";
+                        SecondArgumentTextBox.Text = "";
+                        break;
+                }
             }
             catch
             {
                 FirstArgumentTextBox.Text = "";
                 SecondArgumentTextBox.Text = "";
             }
-
         }
     }
 }
