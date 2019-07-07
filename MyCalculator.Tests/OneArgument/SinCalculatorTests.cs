61185ccab8f4d361a50d468e2901302ea6797e12
VladimirTsarev1 <vladimir.csarev@gmail.com>
2019-07-07 09:43:20 +0600

    Make advanced test

---
+++ MyCalculator.Tests/OneArgument/SinCalculatorTests.cs
@@ -0,0 +1,22 @@
+﻿using NUnit.Framework;
+
+namespace MyCalculator.Tests.OneArgument
+{
+    [TestFixture]
+
+    public class SinCalculatorTests
+    {
+
+        [TestCase(2, 0.909)]
+        [TestCase(1.2, 0.932)]
+        [TestCase(-2, -0.909)]
+        public void CalculateTests(
+            double firstValue,
+            double expected)
+        {
+            SinCalculator sinCalculator = new SinCalculator();
+            double result = sinCalculator.Calculate(firstValue);
+            Assert.AreEqual(expected, result, 0.001);
+        }
+    }
+}
